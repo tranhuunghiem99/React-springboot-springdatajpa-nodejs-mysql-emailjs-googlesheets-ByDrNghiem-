fe3a5e3577ea9a9f735dd7e9312ca3e22e9afa56
tranhuunghiem99 <tranhuunghiem48@gmail.com>
2021-06-20 07:26:27 +0700

    up node js

--- BE_JAVA/Nghiem-KSCTDT/src/main/java/com/Nghiem/tgu/models/ERole.java
+++
@@ -1,7 +0,0 @@
-package com.Nghiem.tgu.models;
-
-public enum ERole {
-	ROLE_USER,
-    ROLE_MODERATOR,
-    ROLE_ADMIN
-}
--- BE_JAVA/NghiemKSspring/src/main/java/com/Nghiem/tgu/Model/ERole.java
+++
@@ -1,7 +0,0 @@
-package com.Nghiem.tgu.Model;
-
-public enum ERole {
-	ROLE_USER,
-    ROLE_MODERATOR,
-    ROLE_ADMIN
-}
